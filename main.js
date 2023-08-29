81a0f66d89975f631d6404b73dfd41212f246c63
poonamnikam1999 <pnikam2212@gmail.com>
2023-08-29 15:51:58 +0530

    add files

---
+++ main.js
@@ -0,0 +1,9 @@
+// let nav=document.querySelector(".header");
+// window.onscroll=function (){
+//     if(document.documentElement.scrollTop > 20){
+//         nav.classList.add("scroll-on");
+//     }
+//     else{
+//         nav.classList.remove("scroll-on");
+//     }
+// }
